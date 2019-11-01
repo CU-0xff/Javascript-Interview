b7144ad221808431146e18b8757c5b0604cf42a7
Frank Fischer <frank@fischer.com.mt>
2019-11-01 09:57:17 +0100

    Own Excercise

---
+++ OwnExcercise1.js
@@ -0,0 +1,11 @@
+var Vehicle = function (license="", vendor="", age=0, cost=30000) {
+    this.license = license;
+    this.vendor = vendor;
+    this.age = age;
+    this.cost = cost;
+
+    this.estValue = function() {
+        return (1-Math.tanh(this.age/10))*this.cost;
+    }
+
+}
